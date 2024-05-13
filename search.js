31a72372d1d5568e0dd871d92c0276fdb791dcf8
peanutbutterjlly <steverios_sr@yahoo.com>
2024-05-13 10:20:34 -0500

    install: new langchain package; add: semantic search functionality

--- search.js
+++ search.js
@@ -1,7 +1,7 @@
+import { OpenAIEmbeddings } from '@langchain/openai';
+import 'dotenv/config';
 import { Document } from "langchain/document";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { openai } from './openai.js';
 import { movies } from "./movies.js";
 
 /**
@@ -21,3 +21,17 @@ function createStore() {
     new OpenAIEmbeddings()
   );
 }
+
+/**
+ * Asynchronously searches for similar documents in the store based on the given query.
+ *
+ * @param {string} query - The query string to search for.
+ * @param {number} [count=1] - The number of similar documents to return. Defaults to 1.
+ * @return {Promise<Array<Object>>} A promise that resolves to an array of similar documents.
+ */
+async function search(query, count=1) {
+  const store = await createStore();
+  return store.similaritySearch(query, count);
+}
+
+console.log(await search('thought provoking'));
