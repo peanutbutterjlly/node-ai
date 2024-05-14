30c68d81c04570ebde7a2a6da3db0100c9b497a3
peanutbutterjlly <steverios_sr@yahoo.com>
2024-05-14 11:47:40 -0500

    added some packages for YT and PDF parsing; created store func

--- qa.js
+++ qa.js
@@ -6,3 +6,15 @@ import { CharacterTextSplitter } from 'langchain/text_splitter';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 import { openai } from './openai.js';
 
+const question = process.argv[2] || 'hi';
+const video = 'https://youtu.be/zR_iuq2evXo?si=cG8rODgRgXOx9_Cn';
+
+/**
+ * A function that creates a MemoryVectorStore from the given documents using OpenAI embeddings.
+ *
+ * @param {type} docs - Description of the documents parameter
+ * @return {type} Description of the return value
+ */
+export function createStore(docs) {
+  return MemoryVectorStore.fromDocuments(docs, new OpenAIEmbeddings());
+}
