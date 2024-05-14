d7882121bf33841656eaeed24fb4a02ddc1f6d69
peanutbutterjlly <steverios_sr@yahoo.com>
2024-05-14 12:59:34 -0500

    get docs from YT and PDF funcs

--- qa.js
+++ qa.js
@@ -18,3 +18,51 @@ const video = 'https://youtu.be/zR_iuq2evXo?si=cG8rODgRgXOx9_Cn';
 export function createStore(docs) {
   return MemoryVectorStore.fromDocuments(docs, new OpenAIEmbeddings());
 }
+
+/**
+ * Loads and splits text from a YouTube video based on the provided URL.
+ *
+ * @param {string} video - The URL of the YouTube video.
+ * @return {type} Description of the return value
+ */
+export function docsFromYTVideo(video) {
+  const loader = YoutubeLoader.createFromUrl(video, {
+    language: 'en',
+    addVideoInfo: true,
+  })
+  return loader.loadAndSplit(
+    new CharacterTextSplitter({
+      separator: ' ',
+      chunkSize: 2500,
+      chunkOverlap: 100,
+    })
+  )
+}
+
+/**
+ * Loads and splits text from a PDF document.
+ *
+ * @return {type} Description of the return value
+ */
+function docsFromPDF() {
+  const loader = new PDFLoader('xbox.pdf');
+  return loader.loadAndSplit(
+    new CharacterTextSplitter({
+      separator: '. ',
+      chunkSize: 2500,
+      chunkOverlap: 200,
+    })
+  )
+}
+
+/**
+ * Asynchronously loads video and PDF documents, then creates a store from the combined documents.
+ *
+ * @return {Promise<MemoryVectorStore>} A Promise that resolves to the created MemoryVectorStore.
+ */
+async function loadStore() {
+  const videoDocs = await docsFromYTVideo(video);
+  const pdfDocs = await docsFromPDF();
+
+  return createStore([...videoDocs, ...pdfDocs])
+}
